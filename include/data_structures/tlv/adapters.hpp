9f959bd6f3cfa4af265955825a5bb59b79d68100
Lea Yeh <lea.yeh.ml@gmail.com>
2025-08-19 13:58:19 +0000

    feat: Export data buffer stream api which call tlv

---
+++ include/data_structures/tlv/adapters.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "data_buffer.hpp"
+#include "tlv.hpp"
+
+// DataBuffer or any others *Buffer do not need to know TLV
+// register in the header
+
+// format:
+//  template <class T> *Buffer& operator<<(*Buffer& out, const T& v)
+//  template <class T> *Buffer& operator>>(*Buffer& in, T& v)
+
+template <class T> DataBuffer& operator<<(DataBuffer& out, const T& v)
+{
+    tlv::write(out, v);
+    return out;
+}
+
+template <class T> DataBuffer& operator>>(DataBuffer& in, T& v)
+{
+    tlv::read(in, v);
+    return in;
+}
