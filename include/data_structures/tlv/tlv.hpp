d30b09918ac5f92f42e88a563e9f4795ab2deccc
Lea Yeh <lea.yeh.ml@gmail.com>
2025-08-19 08:36:04 +0000

    refactor: Separate tlv implementation to reader and writer

---
+++ include/data_structures/tlv/tlv.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include "tlv_reader.hpp"
+#include "tlv_type.hpp"
+#include "tlv_writer.hpp"
--- tests/tlv_test.cpp
+++ tests/tlv_test.cpp
@@ -1,7 +1,6 @@
-#include "tlv.hpp"
+#include "tlv/tlv.hpp"
 #include <gtest/gtest.h>
 
-using namespace tlv;
 
 TEST(TLV, VarIntBoundary)
 {
@@ -13,12 +12,44 @@ TEST(TLV, VarIntBoundary)
 
     for (auto [num, nBytes] : tbl) {
         std::vector<std::byte> buf;
-        putVarint(buf, num);
+        tlv::Writer::writeVarint(buf, num);
         EXPECT_EQ(buf.size(), nBytes);
 
         const std::byte* cur = buf.data();
-        uint64_t out = getVarint(cur);
+        uint64_t out = tlv::Reader::readVarint(cur);
         EXPECT_EQ(out, num);
         EXPECT_EQ(cur, buf.data() + nBytes);
     }
 }
+
+// TEST(TLV, FixedEndianRoundTrip32)
+// {
+//     std::vector<std::byte> buf;
+//     writeFixed<std::uint32_t>(buf, 0x12345678u);
+//     const std::byte* p = buf.data();
+//     auto v = readFixed<std::uint32_t>(p);
+//     EXPECT_EQ(v, 0x12345678u);
+// }
+
+// TEST(TLV, FixedEndianRoundTrip64)
+// {
+//     std::vector<std::byte> buf;
+//     writeFixed<std::uint64_t>(buf, 0x1122334455667788ULL);
+//     const std::byte* p = buf.data();
+//     auto v = readFixed<std::uint64_t>(p);
+//     EXPECT_EQ(v, 0x1122334455667788ULL);
+// }
+
+// TEST(TLV, BytesBlock)
+// {
+//     std::vector<std::byte> payload(200);
+//     std::iota(payload.begin(), payload.end(), std::byte{0});
+//     std::vector<std::byte> buf;
+//     writeLenBytes(buf, payload.data(), payload.size());
+
+//     const std::byte* p = buf.data();
+//     size_t len{};
+//     const std::byte* start = readLenBytes(p, len);
+//     ASSERT_EQ(len, payload.size());
+//     EXPECT_TRUE(std::equal(start, start+len, payload.begin()));
+// }
