cc0c2898252845e7e0b7a68376524ff299d123e7
LeaYeh <smile2140@gmail.com>
2025-08-29 20:08:57 +0000

    feat: Add type traits to detect the type of containers

--- include/data_structures/tlv_type_traits.hpp
+++ include/data_structures/tlv_type_traits.hpp
@@ -1,9 +1,21 @@
 #pragma once
+#include <array>
 #include <cstddef>
 #include <cstdint>
+#include <deque>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
 #include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
-template<class> inline constexpr bool dependent_false_v = false;
+template <class> inline constexpr bool dependent_false_v = false;
 
 class ByteIO;
 
@@ -34,3 +46,60 @@ inline constexpr bool raw_byte_like_v =
     || std::is_same_v<std::remove_cv_t<T>, unsigned char>
     || std::is_same_v<std::remove_cv_t<T>, std::uint8_t>
     || std::is_same_v<std::remove_cv_t<T>, char>;
+
+// basic string / array
+template <class T> struct is_basic_string : std::false_type
+{
+};
+template <class Ch, class Tr, class Al>
+struct is_basic_string<std::basic_string<Ch, Tr, Al>> : std::true_type
+{
+};
+
+template <class T> struct is_std_array : std::false_type
+{
+};
+
+template <class T, std::size_t N>
+struct is_std_array<std::array<T, N>> : std::true_type
+{
+};
+
+// detect container shape likes
+template <class T>
+concept has_value_type = requires { typename T::value_type; };
+
+template <class T>
+concept has_key_type = requires { typename T::key_type; };
+
+template <class T>
+concept has_mapped_type = requires { typename T::mapped_type; };
+
+template <class T, class = void> struct has_begin_end : std::false_type
+{
+};
+template <class T>
+struct has_begin_end<T,
+                     std::void_t<decltype(std::begin(std::declval<T&>())),
+                                 decltype(std::end(std::declval<T&>()))>> :
+    std::true_type
+{
+};
+
+template <class T>
+inline constexpr bool is_basic_string_v = is_basic_string<T>::value;
+template <class T>
+inline constexpr bool is_std_array_v = is_std_array<T>::value;
+template <class T>
+inline constexpr bool has_begin_end_v = has_begin_end<T>::value;
+
+template <class T>
+inline constexpr bool is_map_like_v = has_key_type<T> && has_mapped_type<T>;
+
+template <class T>
+inline constexpr bool is_set_like_v = has_key_type<T> && !has_mapped_type<T>;
+
+template <class T>
+concept is_container_like = requires {
+    typename T::value_type;
+} && has_begin_end_v<T> && !is_basic_string_v<std::remove_cvref_t<T>>;
