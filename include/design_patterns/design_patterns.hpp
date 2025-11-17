34be15620636c191af8db3adb07c6162ff6d134b
Lea Yeh <lea.yeh.ml@gmail.com>
2025-11-17 20:35:31 +0000

    chore: Add includes into module header

--- include/design_patterns/design_patterns.hpp
+++ include/design_patterns/design_patterns.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
 #include "design_patterns/memento/memento.hpp"
+#include "design_patterns/observer.hpp"
+#include "design_patterns/singleton.hpp"
+#include "design_patterns/state_machine.hpp"
