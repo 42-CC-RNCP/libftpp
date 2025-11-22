e5135218b19b968f74f2c0782c37f9dd94b1ced6
Lea Yeh <lea.yeh.ml@gmail.com>
2025-11-22 23:54:06 +0000

    opt: Fix set prefix timing and check thread state

--- include/threading/thread.hpp
+++ include/threading/thread.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "iostream/thread_safe_iostream.hpp"
+#include <atomic>
 #include <functional>
+#include <stdexcept>
 #include <string>
 #include <thread>
 
@@ -9,7 +11,15 @@ class Thread
 public:
     void start()
     {
+        // exception should be thrown if start() is called more than once
+        if (started_.exchange(true)) {
+            throw std::runtime_error("Thread '" + name_
+                                     + "' has already been started.");
+        }
         thread_ = std::thread([this]() {
+            // here is the worker thread
+            // if the exception is thrown, it will call std::terminate 
+            ts_cout.setPrefix("[" + name_ + "] ");
             function_();
         });
     }
@@ -26,12 +36,19 @@ public:
     {
         name_ = name;
         function_ = functToExecute;
-        tsiostream_.setPrefix("[" + name_ + "] ");
     }
+
+    ~Thread() { stop(); }
+    // Disable copy and move semantics, as threads should not be copied or
+    // moved.
+    Thread(const Thread&) = delete;
+    Thread& operator=(const Thread&) = delete;
+    Thread(Thread&&) = delete;
+    Thread& operator=(Thread&&) = delete;
 
 private:
     std::string name_;
     std::thread thread_;
+    std::atomic<bool> started_{false};
     std::function<void()> function_;
-    ThreadSafeIOStream tsiostream_;
 };
