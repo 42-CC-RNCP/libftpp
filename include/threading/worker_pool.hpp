30ec8ec022ea366b5b35ee6fbf42d5c0b2d499e9
Lea Yeh <lea.yeh.ml@gmail.com>
2025-11-30 16:55:03 +0000

    chore: Separate implementation to cpp file for worker pool

--- include/threading/worker_pool.hpp
+++ include/threading/worker_pool.hpp
@@ -2,8 +2,6 @@
 #pragma once
 #include "thread.hpp"
 #include "thread_safe_queue.hpp"
-#include <atomic>
-#include <condition_variable>
 #include <functional>
 #include <memory>
 
@@ -27,66 +25,20 @@ public:
         virtual void execute() = 0;
     };
 
-    WorkerPool(size_t numberOfWorkers = 8) : numberOfWorkers_(numberOfWorkers)
-    {
-        // default number of worker threads
-        // create worker threads
-        for (size_t i = 0; i < numberOfWorkers_; ++i) {
-            workers_.emplace_back(std::make_unique<Thread>(
-                "worker_" + std::to_string(i), [this]() {
-                    while (true) {
-                        // fetch job from the job queue
-                        auto job = jobQueue_.pop_front_optional();
-                        if (job.has_value() == false) {
-                            // job queue is closed and empty, exit the worker
-                            // thread
-                            break;
-                        }
-                        // execute the job
-                        job->get()->execute();
-                    }
-                }));
-            workers_.back()->start();
-        }
-    }
+    WorkerPool(size_t numberOfWorkers = 8);
+    ~WorkerPool();
 
-    ~WorkerPool() { stop(); }
-
-    void addJob(const std::function<void()>& jobToExecute)
-    {
-        class JobImpl : public IJob
-        {
-        public:
-            JobImpl(const std::function<void()>& jobFunc) : jobFunc_(jobFunc) {}
-            void execute() override { jobFunc_(); }
-
-        private:
-            std::function<void()> jobFunc_;
-        };
-
-        // the job queue is thread-safe, just push the new job
-        jobQueue_.push_back(std::make_shared<JobImpl>(jobToExecute));
-    }
+    void addJob(const std::function<void()>& jobToExecute);
 
     // wait for all worker threads to finish
-    void joinAllWorkers()
-    {
-        for (auto& worker : workers_) {
-            worker->stop();
-        }
-    }
+    void joinAllWorkers();
 
     // stop accepting new jobs and wait for all worker threads to finish
-    void stop()
-    {
-        jobQueue_.close();
-        joinAllWorkers();
-    }
+    void stop();
 
 private:
     size_t numberOfWorkers_;
-    // job queue shared between main thread and worker threads, so use
-    // shared_ptr
+    // 
     ThreadSafeQueue<std::shared_ptr<IJob>> jobQueue_;
     std::vector<std::unique_ptr<Thread>> workers_;
 };
