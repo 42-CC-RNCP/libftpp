d2fba6fe0f9b822d057fe7a887d74896bcd7f9c1
Lea Yeh <lea.yeh.ml@gmail.com>
2025-12-08 20:53:51 +0000

    feat: Add basic implementation of dispatch class

--- include/network/dispatcher.hpp
+++ include/network/dispatcher.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "client_handle.hpp"
+// #include "data_structures/data_buffer.hpp"
 #include "message.hpp"
 #include <functional>
+#include <utility>
 
 using RawHandler = std::function<void(ClientHandle&, const Message&)>;
 
@@ -9,13 +11,41 @@ using RawHandler = std::function<void(ClientHandle&, const Message&)>;
 class Dispatcher
 {
 public:
-    void defineRawAction(Message::Type t, RawHandler h);
+    void defineRawAction(Message::Type t, RawHandler h)
+    {
+        if (handlers_.count(t)) {
+            throw std::runtime_error(
+                "Handler for this message type already defined");
+        }
+        handlers_[t] = std::move(h);
+    }
 
-    // define a handler for a specific message type T
-    template <typename T>
-    void defineTypedAction(Message::Type t,
-                           std::function<void(ClientHandle&, const T&)> h);
-    void dispatch(ClientHandle& cli, const Message& msg);
+    // // define a handler for a specific message type T
+    // template <typename T>
+    // void
+    // defineTypedAction(Message::Type type,
+    //                   std::function<void(ClientHandle&, const T&)>
+    //                   typedHandler)
+    // {
+    //     defineRawAction(type,
+    //                     [typedHandler](ClientHandle& cli, const Message& msg)
+    //                     {
+    //                         T obj;
+
+    //                     });
+    // }
+
+    void dispatch(ClientHandle& cli, const Message& msg) const
+    {
+        auto it = handlers_.find(msg.type());
+        if (it != handlers_.end()) {
+            it->second(cli, msg);
+        }
+        else {
+            throw std::runtime_error(
+                "No handler defined for this message type");
+        }
+    }
 
 private:
     std::unordered_map<Message::Type, RawHandler> handlers_;
