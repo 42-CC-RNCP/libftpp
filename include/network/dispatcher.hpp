14a7c365ab96425c34cf78495394cb0948a48b61
Lea Yeh <lea.yeh.ml@gmail.com>
2026-01-12 09:27:27 +0000

    refactor: Make dispatcher as template to fit with different context

--- include/network/dispatcher.hpp
+++ include/network/dispatcher.hpp
@@ -1,52 +1,31 @@
 #pragma once
-#include "client_handle.hpp"
-// #include "data_structures/data_buffer.hpp"
 #include "message.hpp"
 #include <functional>
 #include <utility>
 
-using RawHandler = std::function<void(ClientHandle&, const Message&)>;
+template <typename Context>
+using RawHandlerT = std::function<void(Context&, const Message&)>;
 
-// map message types to their handlers
-class Dispatcher
+template <typename Context> class DispatcherT
 {
 public:
-    void defineRawAction(Message::Type t, RawHandler h)
+    void defineRawAction(Message::Type t, RawHandlerT<Context> h)
     {
         if (handlers_.count(t)) {
-            throw std::runtime_error(
-                "Handler for this message type already defined");
+            throw std::runtime_error("Handler already defined");
         }
         handlers_[t] = std::move(h);
     }
 
-    // // define a handler for a specific message type T
-    // template <typename T>
-    // void
-    // defineTypedAction(Message::Type type,
-    //                   std::function<void(ClientHandle&, const T&)>
-    //                   typedHandler)
-    // {
-    //     defineRawAction(type,
-    //                     [typedHandler](ClientHandle& cli, const Message& msg)
-    //                     {
-    //                         T obj;
-
-    //                     });
-    // }
-
-    void dispatch(ClientHandle& cli, const Message& msg) const
+    void dispatch(Context& ctx, const Message& msg) const
     {
         auto it = handlers_.find(msg.type());
-        if (it != handlers_.end()) {
-            it->second(cli, msg);
+        if (it == handlers_.end()) {
+            throw std::runtime_error("No handler defined");
         }
-        else {
-            throw std::runtime_error(
-                "No handler defined for this message type");
-        }
+        it->second(ctx, msg);
     }
 
 private:
-    std::unordered_map<Message::Type, RawHandler> handlers_;
+    std::unordered_map<Message::Type, RawHandlerT<Context>> handlers_;
 };
