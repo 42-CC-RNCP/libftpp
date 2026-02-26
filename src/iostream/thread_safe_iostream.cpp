5fce748269ad450be003d0098a5febc644267d8b
Lea Yeh <lea.yeh.ml@gmail.com>
2026-02-26 14:58:48 +0000

    fix: Keep the template impl in header file

--- src/iostream/thread_safe_iostream.cpp
+++ src/iostream/thread_safe_iostream.cpp
@@ -1,28 +1,8 @@
 #include "iostream/thread_safe_iostream.hpp"
-#include <stdexcept>
 
 void ThreadSafeIOStream::setPrefix(const std::string& prefix)
 {
     prefix_ = prefix;
-}
-
-template <typename T>
-void ThreadSafeIOStream::prompt(const std::string& question, T& dest)
-{
-    // std::lock_guard only locks for the duration of its scope
-    {
-        // lock cout to print question
-        std::lock_guard<std::mutex> lock(g_cout_mutex);
-        std::cout << prefix_ << question << std::endl;
-        // cout lock released here
-    }
-    {
-        // lock cin to read answer
-        std::lock_guard<std::mutex> lock(g_cin_mutex);
-        std::cin >> dest;
-        // cin lock released here
-    }
-    buffer_ << dest;
 }
 
 void ThreadSafeIOStream::flush()
@@ -37,26 +17,6 @@ void ThreadSafeIOStream::flush()
         buffer_.str(""); // Clear the buffer
         buffer_.clear(); // Clear any error flags
     }
-}
-
-// Overloaded operators
-template <typename T>
-ThreadSafeIOStream& ThreadSafeIOStream::operator<<(const T& value)
-{
-    buffer_ << value;
-    _flushCompletedLines();
-    return *this;
-}
-
-template <typename T>
-ThreadSafeIOStream& ThreadSafeIOStream::operator>>(T& value)
-{
-    {
-        std::lock_guard<std::mutex> lock(g_cin_mutex);
-        std::cin >> value;
-    }
-    buffer_ << value;
-    return *this;
 }
 
 // Handle manipulators like std::endl, std::flush, ends, etc.
@@ -77,20 +37,19 @@ ThreadSafeIOStream::operator<<(std::ios& (*manip)(std::ios&))
 }
 
 void ThreadSafeIOStream::_flushCompletedLines()
-    {
-        std::string content = buffer_.str();
-        size_t pos = 0;
+{
+    std::string content = buffer_.str();
+    size_t pos = 0;
 
-        while ((pos = content.find('\n')) != std::string::npos) {
-            std::string line = content.substr(0, pos);
-            {
-                std::lock_guard<std::mutex> lock(g_cout_mutex);
-                std::cout << prefix_ << line << std::endl;
-            }
-            content.erase(0, pos + 1);
+    while ((pos = content.find('\n')) != std::string::npos) {
+        std::string line = content.substr(0, pos);
+        {
+            std::lock_guard<std::mutex> lock(g_cout_mutex);
+            std::cout << prefix_ << line << std::endl;
         }
-        buffer_.str(content);
-        buffer_.clear();                 // Clear any error flags
-        buffer_.seekp(0, std::ios::end); // Move the put pointer to the end
+        content.erase(0, pos + 1);
     }
-
+    buffer_.str(content);
+    buffer_.clear();                 // Clear any error flags
+    buffer_.seekp(0, std::ios::end); // Move the put pointer to the end
+}
